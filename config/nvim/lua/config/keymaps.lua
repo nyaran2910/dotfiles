2c8a2117e688e998bce930d2748f4fc1d28359f9
nyaran <Nyaran2910@gmail.com>
2026-04-24 11:51:17 +0900

    updated

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,31 @@
 -- Add any additional keymaps here
 vim.keymap.set("n", "Q", "<leader>bd", { remap = true, desc = "Close Buffer" })
 vim.keymap.set("t", "<C-h>", "<BS>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>yc", function()
+  local path = vim.fn.expand("%:.")
+
+  -- Visualかどうか判定
+  local mode = vim.fn.mode()
+
+  local text
+
+  if mode == "v" or mode == "V" or mode == "\22" then
+    -- Visual選択範囲
+    local start_line = vim.fn.getpos("v")[2]
+    local end_line = vim.fn.getpos(".")[2]
+
+    -- 順序保証（逆方向選択対策）
+    if start_line > end_line then
+      start_line, end_line = end_line, start_line
+    end
+
+    text = string.format("%s:%d-%d", path, start_line, end_line)
+  else
+    -- 通常モード
+    local line = vim.fn.line(".")
+    text = string.format("%s:%d", path, line)
+  end
+
+  vim.fn.setreg("+", text)
+  print("Copied: " .. text)
+end, { desc = "Copy file:line or range" })
