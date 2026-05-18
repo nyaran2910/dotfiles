213f534b97839264386860bbcb30bcf3bf07e313
nyaran <Nyaran2910@gmail.com>
2026-05-19 07:20:02 +0900

    chore

---
+++ config/nvim/lua/plugins/mdmath.lua
@@ -0,0 +1,16 @@
+return {
+  "Thiago4532/mdmath.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {
+    filetypes = { "markdown" },
+    foreground = "Normal",
+    anticonceal = true,
+    hide_on_insert = true,
+    dynamic = true,
+    dynamic_scale = 1.0,
+    update_interval = 400,
+    internal_scale = 1.0,
+  },
+}
